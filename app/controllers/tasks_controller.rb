8c1dda3213dac4b2bcb35845a51cc66cf3b55a9c
shuuichirou.morioka <LIARCry079@gmail.com>
2020-12-23 13:53:44 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,9 +55,7 @@ class TasksController < ApplicationController
  
  private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
 
   def task_params
     params.require(:task).permit(:content, :status)
